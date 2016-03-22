5dbf10f49f01ed1a1ab488eed18163b85d4837d1
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2016-03-22 14:58:34 +0500

    Remove all css files from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -37,34 +37,24 @@ module.exports = {
   ],
   module: {
     loaders: [
-      {
-        test: /\.less/,
-        loader: 'style!css!less'
-      },
-      {test: /\.css$/, loader: 'style-loader!css-loader!cssnext-loader'},
       {test: /\.(js|jsx)$/, loaders: ['babel'], include: path.join(__dirname, 'lib')},
 
       // Required for bootstrap loader
       { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
-      { test: /fullcalendar/, loader: 'imports?$=jquery,moment' },
 
-      { test: /\.(woff|woff2)/,  loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-      { test: /\.ttf/,    loader: "file-loader" },
-      { test: /\.eot/,    loader: "file-loader" },
-      { test: /\.svg/,    loader: "file-loader" },
+      { test: /\.(woff|woff2)/,  loader: "empty-loader" },
+      { test: /\.ttf/,    loader: "empty-loader" },
+      { test: /\.eot/,    loader: "empty-loader" },
+      { test: /\.svg/,    loader: "empty-loader" },
 
       // Load images
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
-          'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          'empty-loader'
         ]
       }
     ]
-  },
-  cssnext: {
-    browsers: 'last 2 versions'
   },
   resolve: {
    extensions: ['', '.js', '.jsx']
